cce2e6b004f05e6d3b142711768d801d8f72601a
abir2010 <kaiserabir222@gmail.com>
2023-11-21 12:36:35 +0600

    firebase setup complete

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,7 +6,7 @@ export default function Video() {
     return (
         <Link to="/quiz">
             <div className={Classes.video}>
-                <img src={image} alt />
+                <img src={image} alt="Alt Tag" />
                 <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
                 <div className={Classes.qmeta}>
                     <p>10 Questions</p>
